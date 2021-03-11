9be9360efc8b969f2119a32cb1778525dd8cb82b
Louis Lafontaine <louishenri.lafontaine@gmail.com>
2021-03-11 18:51:41 +0100

    Modif mise en place basique d'une balle qui peut se déplacer dans la fenêtre de jeu

--- src/PanelJeu.java
+++ src/PanelJeu.java
@@ -1,14 +1,65 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class PanelJeu extends JPanel {
+public class PanelJeu extends JPanel implements ActionListener, MouseListener{
+    // Attributs
+    private final Balle balle;
+    protected Timer timer;
+
+    // Constructeur
     public PanelJeu(){
+        balle = new Balle(100,250,20,5);
+
+        int fps = 60;
+        timer = new Timer(1000/ fps, this);
+
+        addMouseListener(this);
     }
 
+    // Méthodes
+    @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.setColor(Color.red);
-        g.fillRect(0,0,this.getWidth(),this.getHeight());
+        g.fillRect(0,0, this.getWidth(), this.getHeight());
+        balle.drawBalle(g);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == timer){
+            balle.updatePosBalle(this.getWidth(), this.getHeight());
+            repaint();
+        }
+    }
+
+    // Mouse Listener interface method
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        timer.start();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        timer.stop();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
