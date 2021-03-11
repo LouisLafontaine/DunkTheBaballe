dfe93290e2a1bda14a3def13a5429074dff5e56f
Louis Lafontaine <louishenri.lafontaine@gmail.com>
2021-03-11 18:50:31 +0100

    Création objet Balle

--- src/Balle.java
+++ src/Balle.java
@@ -1,2 +1,46 @@
+import java.awt.*;
+
 public class Balle {
+    // Attributs
+    protected int x; // pos x
+    protected int y; // pos y
+    protected int xInit ; // pos x initiale
+    protected int yInit ; // pos y initiale
+    protected int r; // rayon
+    protected double v; // vitesse
+    protected Color maCouleur; // couleur
+
+    // Constructeur
+    public Balle(int x, int y, int r, double v) {
+        this.x = x;
+        this.y = y;
+        this.xInit = x;
+        this.yInit = y;
+        this.r = r;
+        this.v = v;
+        maCouleur = new Color(100, 200,200);
+    }
+
+    // Méthodes
+    public void drawBalle(Graphics g) {
+        g.setColor(maCouleur);
+        g.fillOval(x, y, r,r);
+    }
+
+    public void updatePosBalle(int largeurFenetre, int hauteurFenetre){
+        x = (int) (x+v);
+//        y = (int) (y+v);
+        if(notInBounds(largeurFenetre,hauteurFenetre)){
+            resetPosBalle();
+        }
+    }
+
+    public void resetPosBalle(){
+        x = xInit;
+        y = yInit;
+    }
+
+    public boolean notInBounds(int largeurFenetre, int hauteurFenetre) { // true si hors dans la fenetre
+        return (this.x > largeurFenetre || this.y > hauteurFenetre);
+    }
 }
